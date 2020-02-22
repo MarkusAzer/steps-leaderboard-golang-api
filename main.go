c0062fb6ed76955a99f58f7521975c9a55120fbf
Markus <markusgihady@gmail.com>
2020-02-22 10:34:40 +0200

    [ADD] init api

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	http.Handle("/", r)
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+
+}
